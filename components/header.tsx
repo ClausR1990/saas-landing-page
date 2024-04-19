c520c2b637a1a41c92865b19b550fd48905de502
Claus Reinhold <claus.reinhold@gmail.com>
2024-04-19 10:42:57 +0200

    link on logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { BadgeDollarSign } from 'lucide-react'
 import { Navigation } from './navigation'
 import { ModeToggle } from './ui/mode-toggle'
@@ -6,7 +7,10 @@ export const Header = () => {
   return (
     <header className='gradient-bottom fixed z-[1000] w-full border-b border-muted p-4 transition-all duration-150 hover:bg-background'>
       <div className='container flex max-w-full flex-wrap items-center justify-between'>
-        <BadgeDollarSign />
+        <Link href='/'>
+          <BadgeDollarSign />
+          <span className='sr-only'>Gå til forsiden</span>
+        </Link>
         <Navigation />
         <div>
           <ModeToggle />
